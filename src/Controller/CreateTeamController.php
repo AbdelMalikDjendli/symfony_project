565be99419ff65f3c81647fd8b1676d9e76de64a
Imane <imanemeradi3@gmail.com>
2023-02-02 20:24:01 +0100

    creation form create team

--- src/Controller/CreateTeamController.php
+++ src/Controller/CreateTeamController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\FormHandler\CreateTeamHandlerr;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Team;
+use App\Entity\User;
+use App\Form\CreateTeamType;
 
 class CreateTeamController extends AbstractController
 {
@@ -13,6 +18,25 @@ class CreateTeamController extends AbstractController
     {
         return $this->render('create_team/index.html.twig', [
             'controller_name' => 'CreateTeamController',
+        ]);
+    }
+
+    #[Route('/user/{id}/createteam/', name: 'app_create_team', methods: ['GET', 'POST'])]
+    public function create(Request $request, CreateTeamHandlerr $createTeamHandler, User $user): Response
+    {
+        $equipe = new Team();
+
+        $form = $this->createForm(CreateTeamType::class, $equipe);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            echo "form envoyé";
+            $createTeamHandler->handleForm($equipe, $user);
+            # rediriger maintenant le formulaire (une fois envoyé) vers la page d'accueil ou sur la page du match
+        }
+
+        return $this->render('create_team/createteam.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Form/CreateTeamType.php
+++ src/Form/CreateTeamType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Team;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +14,15 @@ class CreateTeamType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('player1')
-            ->add('player2')
-            ->add('player3')
-            ->add('player4')
-            ->add('player5')
-            ->add('name')
-            ->add('match')
-            ->add('creator')
+            ->add('name', TextType::class, ['required' => true, 'label' => 'Nom de l\'équipe', ])
+            ->add('player1', TextType::class,['required' => true, 'label' => 'Joueur 1', ] )
+            ->add('player2', TextType::class, ['required' => true, 'label' => 'Joueur 2', ])
+            ->add('player3', TextType::class, ['required' => true, 'label' => 'Joueur 3', ])
+            ->add('player4', TextType::class, ['required' => true, 'label' => 'Joueur 4', ])
+            ->add('player5', TextType::class,['required' => true, 'label' => 'Joueur 5', ])
+            ->add('submit', SubmitType::class, ['label' => 'Crée l\'équipe',])
+
+
         ;
     }
 
