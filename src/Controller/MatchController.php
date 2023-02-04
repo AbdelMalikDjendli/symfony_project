d4eca79df0c6caadef0c9d73571b774f67b91f2a
DeveloppementWeb19 <pierre-louis.clavel@laposte.net>
2023-02-04 15:17:40 +0100

    form create match

---
+++ src/Controller/MatchController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Event;
+use App\Entity\User;
+use App\Form\MatchCreatorType;
+use App\FormHandler\MatchFormHandler;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MatchController extends AbstractController
+{
+
+    public function __construct(public EntityManagerInterface $entityManager)
+    {
+    }
+
+    #[Route('/match/create', name: 'app_match_create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager, MatchFormHandler $matchFormHandler): Response
+    {
+        $userRepository = $entityManager -> getRepository(User::class);
+
+        # appel de l'utilisateur connecté
+        $mail = $this->getUser()->getUserIdentifier();
+
+        # récupération de l'entité user
+        $user = $userRepository -> findOneBy(["email" => $mail]);
+
+        # instanciation du match (vide)
+        $match = new Event();
+
+        #
+        $form = $this->createForm(MatchCreatorType::class, $match);
+
+        # le formulaire saisit la requête
+        $form->handleRequest($request);
+
+        # lorsque la requête est envoyée et vérifiée
+        if ($form->isSubmitted() && $form->isValid()) {
+            echo "form envoyé";
+
+            # gestion des données reçues
+            $matchFormHandler->handleForm($match, $user);
+
+            # rediriger maintenant le formulaire (une fois envoyé) vers la page d'accueil ou sur la page du match
+        }
+
+        # affichage de la vue du formulaire de création
+        return $this->render('match/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+    }
+
+}
---
+++ src/FormHandler/MatchFormHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\FormHandler;
+use App\Entity\Event;
+
+use App\Entity\Five;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
+final class MatchFormHandler
+{
+    public function __construct(
+        public EntityManagerInterface $entityManager,
+        public ManagerRegistry $doctrine
+    )
+    {
+
+    }
+
+    # Equipe pas encore créée par l'utilisateur
+    public function handleForm(Event $match, User $user): void
+    {
+        # récupère l'utilisateur transmis par le controlleur
+        $match->setOrganizer($user);
+
+
+        $five = $this->defaultFive();
+        $match -> setFive($five);
+
+        $this->entityManager->persist($match);
+        $this->entityManager->flush();
+    }
+
+    # Utilisation d'un utilisateur par défaut déjà créé dans la base de donnée
+    # Cet utilisateur doit être la personne connectée
+    public function defaultUser(): User{
+        return $this->doctrine->getRepository(User::class)->find(1);
+    }
+
+
+    # Utilisation d'un five par défaut déjà créé dans la base de donnée
+    # Ce Five doit être celui qui est sélectionné par l'User dans le formulaire
+    public function defaultFive() : Five{
+
+        return $this->doctrine->getRepository(Five::class)->find(1);
+    }
+}
