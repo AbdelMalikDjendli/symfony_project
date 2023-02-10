fb05c616199842911935b1df7695e0b90aa7a482
DeveloppementWeb19 <pierre-louis.clavel@laposte.net>
2023-02-10 21:37:23 +0100

    affichage des matchs sur la page d'accueil

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/homepage', name: 'app_homepage')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $eventRepository = $entityManager -> getRepository(Event::class);
+        $allMatches = $eventRepository->findAll();
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
+            'matches' => $allMatches
         ]);
     }
 }
