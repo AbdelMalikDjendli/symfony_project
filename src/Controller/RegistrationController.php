c245f80033eb078bab1e0ec367eb86b968c9fa34
Imane <imanemeradi3@gmail.com>
2023-04-01 23:32:50 +0200

    ajt envoi mail ok

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,26 +10,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class RegistrationController extends AbstractController
 {
 
     private $slugger;
+    private $mailer;
 
-    public function __construct(SluggerInterface $slugger)
+    public function __construct(SluggerInterface $slugger, MailerInterface $mailer)
     {
         $this->slugger = $slugger;
+        $this->mailer = $mailer;
+
     }
 
     #[Route('/inscription', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AuthentificationAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AuthentificationAuthenticator $authenticator, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
     {
         $user = new User();
+
+
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
@@ -53,6 +61,7 @@ class RegistrationController extends AbstractController
                 $user->setPhotoFilename($newFilename);
             }
 
+
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
@@ -62,6 +71,26 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
+
+
+                $email = (new Email())
+                    ->from('trouvetonmatchproject@gmail.com')
+                    ->to($user->getEmail())
+                    ->subject('Welcome to our website')
+                    ->html('<p>Cher(e) ' . $user->getPseudo() . ', bienvenu sur TrouveTonMatch !</p>');
+
+                try {
+                    $mailer->send($email);
+                } catch (TransportExceptionInterface $e) {
+                    $this->addFlash('error', 'An error occurred while sending the email. Please try again later or contact the support team.');
+                    throw new \Exception('An error occurred while sending the email.');
+                }
+
+            $this->addFlash('success', 'Your account has been created successfully. Please check your email for further instructions.');
+
+
+
+
             // do anything else you need here, like send an email
 
             return $userAuthenticator->authenticateUser(
@@ -69,6 +98,8 @@ class RegistrationController extends AbstractController
                 $authenticator,
                 $request
             );
+
+
         }
 
         return $this->render('registration/register.html.twig', [
