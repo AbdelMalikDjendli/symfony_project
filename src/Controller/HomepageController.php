d8f8b38eb314d902389097bb76b13834a053625e
DeveloppementWeb19 <pierre-louis.clavel@laposte.net>
2023-04-15 18:34:13 +0200

    refacto twig et services pour les annonces sur la page d'accueil

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Event;
 use App\Entity\User;
 use App\Repository\FiveRepository;
+use App\Services\AnnouncesServices;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -16,69 +17,35 @@ class HomepageController extends AbstractController
 {
 
     public function __construct(public EntityManagerInterface $entityManager,
-                                public FiveRepository $fiveRepository )
+                                public FiveRepository $fiveRepository,
+                                public AnnouncesServices $announcesServices)
     {
     }
 
     #[Route('/homepage/{page}', name: 'app_homepage')]
     public function index(Request $request, int $page = null): Response
     {
-
         if($page === null){
             return $this->redirectToRoute('app_homepage', ['page' => 1]);
         }
-        $eventRepository = $this->entityManager -> getRepository(Event::class);
 
-        //récupère depuis l'url le filtrage des five (méthode GET)
-        $fiveFilter = $request -> get("fives");
-
-        //récupère depuis l'url le filtrage du niveau (méthode GET)
-        $levelFilter = $request -> get("levels");
-
-        //si l'utilisateur est connecté
-        if($this->getUser() != null){
-            # appel de l'utilisateur connecté
-            $mail = $this->getUser()->getUserIdentifier();
-            $userRepository = $this->entityManager -> getRepository(User::class);
-            $user = $userRepository -> findOneBy(["email" => $mail]);
-
-            // les matchs qu'il peut rejoindre lui sont affichés avec les filtres
-            $allMatches = $eventRepository -> findUserJoinableMatches($user, $fiveFilter, $levelFilter);
-        }
-        else {
-            //tous les matchs joignables
-            $allMatches = $eventRepository->findJoinableMatches($fiveFilter, $levelFilter);
-        }
-
+        $allMatches = $this->announcesServices->getAllJoignableMatchs($this->getUser(), $request -> get("fives"), $request -> get("levels"));
+        $matchsToShow = $this->announcesServices->applyPaginationToAnnounces($allMatches, $page, 3);
         $limit = 3;
-        $nbPage =  ceil(count($allMatches)/$limit);
-        $debut = ($page*$limit) - $limit;
-        $allMatches = array_slice($allMatches,$debut, $limit);
 
-        //interception de la requête ajax qui compte les informations du filtrage
         if($request -> get('ajax') == 1){
             return new JsonResponse([
-
-                // renderView retourne le HTML des nouvelles annonces de match
                 'content' => $this->renderView('homepage/main_content.html.twig', [
-                        'controller_name' => 'HomepageController',
-                        'matches' => $allMatches,
-                        'currentPage' => $page,
-                        'nbPage' => $nbPage
+                        'matches' => $matchsToShow, 'currentPage' => $page, 'nbPage' => ceil(count($allMatches)/$limit)
                     ])
             ]);
         }
-
-        $allFives = $this->fiveRepository -> findAll();
-        $allLevels = array('beginner', 'intermediate', 'confirmed');
-
         return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
-            'matches' => $allMatches,
-            'allFives' => $allFives,
-            'allLevels' => $allLevels,
+            'matches' => $matchsToShow,
+            'allFives' => $this->fiveRepository -> findAll(),
+            'allLevels' => array('beginner', 'intermediate', 'confirmed'),
             'currentPage' => $page,
-            'nbPage' => $nbPage
+            'nbPage' => ceil(count($allMatches)/$limit)
         ]);
     }
 }
